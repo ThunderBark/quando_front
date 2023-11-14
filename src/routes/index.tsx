353b9b12e6cc18859276759d68ce74d2f2228c7a
Yenussov Ivan <ivan.enusov@gmail.com>
2023-11-14 22:48:54 +0300

    Добавил папку .vscode в игнор и добавил в README два варианта палетки

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,11 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { Profile } from '../pages/Profile/Profile';
 
 export function RoutesElement() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Profile/>} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
