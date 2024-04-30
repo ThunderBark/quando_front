3a7a1ea90de7c9bfb26c5611fdef1e54a87afdd7
Yenussov Ivan <ivan.enusov@gmail.com>
2024-04-30 19:32:15 +0300

    Корректно отрабатываем изменение положения и размера описания APOD при загрузке страницы и изменении ее размера

--- src/pages/Apod/Showcase/Showcase.tsx
+++ src/pages/Apod/Showcase/Showcase.tsx
@@ -4,40 +4,24 @@ import styles from './Showcase.module.css';
 import { point2d } from "../../../utils/utils";
 
 
-// Хук для отслеживания изменения экрана
-const useWindowSize = () => {
-  const [size, setSize] = useState([0, 0]);
-
-  useLayoutEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
-    }
-    window.addEventListener('resize', updateSize);
-    updateSize();
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
-
-  return size;
-}
-
 export const Showcase = (props: {
   apod: ApodEntry
   onClick?: React.MouseEventHandler<HTMLImageElement>
 }) => {
   const imageId = 'ShowcaseImageId'
+
   const [apod, setApod] = useState(props.apod);
   const [fade_timer, setTimer] = useState(0);
+
   const [imgCalculatedSize, setImgCalculatedSize] = useState<point2d>({x: 0, y: 0});
   const [imgCenter, setImgCenter] = useState<point2d>({x: 0, y: 0});
-
-  const [width, height] = useWindowSize();
 
   const containerRef = useRef<HTMLDivElement>(null);
 
   // Подключаем колбэк при загрузке картинки
   useEffect(() => {
-    document.getElementById(imageId)?.addEventListener('load', imageLoaded)
-    return () => {document.getElementById(imageId)?.removeEventListener('load', imageLoaded)}
+    document.getElementById(imageId)?.addEventListener('load', imageChanged)
+    return () => {document.getElementById(imageId)?.removeEventListener('load', imageChanged)}
   });
 
   // Обработка затенения картинки при переключении
@@ -47,20 +31,26 @@ export const Showcase = (props: {
     containerRef.current?.classList.remove(styles.visible);
     setTimer(setTimeout(() => {
       setApod(props.apod);
-      imageLoaded()
       containerRef.current?.classList.add(styles.visible);
     }, 300));
 
     return () => {clearTimeout(fade_timer)}
   }, [props.apod]);
 
-  // Обновляем положение инфы о картинке при изменении viewport
+  // Обновляем положение инфы о картинке при изменении размера контейнера
   useEffect(() => {
-    imageLoaded();
-  }, [width, height])
+    if (!containerRef.current) return;
+
+    const resizeObserver = new ResizeObserver(() => {
+      imageChanged()
+    });
+    resizeObserver.observe(containerRef.current);
+
+    return () => resizeObserver.disconnect(); // clean up 
+  }, []);
 
   // Изменение положения и размера окна с описанием картинки при загрузке новой картинки
-  const imageLoaded = () => {
+  const imageChanged = () => {
     const image: HTMLImageElement = document.getElementById(imageId)! as HTMLImageElement
     const imageNaturalRatio = image.naturalWidth / image.naturalHeight
     const imageClientRatio = image.clientWidth / image.clientHeight
