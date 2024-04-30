dc9541576f4c87857f31c0a591585f8e1e57b6e8
Yenussov Ivan <ivan.enusov@gmail.com>
2024-04-30 19:11:29 +0300

    Теперь описание APOD находится в рамках картинки

--- src/pages/Apod/Showcase/Showcase.tsx
+++ src/pages/Apod/Showcase/Showcase.tsx
@@ -1,35 +1,95 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useLayoutEffect } from "react";
 import { ApodEntry } from "../ApodAPI";
 import styles from './Showcase.module.css';
+import { point2d } from "../../../utils/utils";
 
+
+// Хук для отслеживания изменения экрана
+const useWindowSize = () => {
+  const [size, setSize] = useState([0, 0]);
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  return size;
+}
 
 export const Showcase = (props: {
   apod: ApodEntry
-  onLoad?: React.ReactEventHandler<HTMLImageElement>
   onClick?: React.MouseEventHandler<HTMLImageElement>
 }) => {
+  const imageId = 'ShowcaseImageId'
   const [apod, setApod] = useState(props.apod);
-  const [timer, setTimer] = useState(0);
+  const [fade_timer, setTimer] = useState(0);
+  const [imgCalculatedSize, setImgCalculatedSize] = useState<point2d>({x: 0, y: 0});
+  const [imgCenter, setImgCenter] = useState<point2d>({x: 0, y: 0});
+
+  const [width, height] = useWindowSize();
 
   const containerRef = useRef<HTMLDivElement>(null);
 
-
+  // Подключаем колбэк при загрузке картинки
   useEffect(() => {
-    clearTimeout(timer);
+    document.getElementById(imageId)?.addEventListener('load', imageLoaded)
+    return () => {document.getElementById(imageId)?.removeEventListener('load', imageLoaded)}
+  });
 
+  // Обработка затенения картинки при переключении
+  useEffect(() => {
+    clearTimeout(fade_timer);
+
+    imageLoaded()
+  
     containerRef.current?.classList.remove(styles.visible);
     setTimer(setTimeout(() => {
       setApod(props.apod);
       containerRef.current?.classList.add(styles.visible);
     }, 300));
 
-    return () => {clearTimeout(timer)}
+    return () => {clearTimeout(fade_timer)}
   }, [props.apod]);
+
+  // Обновляем положение инфы о картинке при изменении viewport
+  useEffect(() => {
+    imageLoaded();
+  }, [width, height])
+
+  // Изменение положения и размера окна с описанием картинки при загрузке новой картинки
+  const imageLoaded = () => {
+    const image: HTMLImageElement = document.getElementById(imageId)! as HTMLImageElement
+    const imageNaturalRatio = image.naturalWidth / image.naturalHeight
+    const imageClientRatio = image.clientWidth / image.clientHeight
+
+    var newImgCalculatedSize: point2d = {x: 0, y: 0}
+    var newImgCenter: point2d = {x: 0, y: 0}
+
+    if (imageNaturalRatio < imageClientRatio) {
+      newImgCalculatedSize.x = image.clientHeight * imageNaturalRatio;
+      newImgCalculatedSize.y = image.clientHeight;
+    }
+    else {
+      newImgCalculatedSize.x = image.clientWidth;
+      newImgCalculatedSize.y = image.clientWidth / imageNaturalRatio;
+    }
+
+    newImgCenter.x = (image.clientWidth - newImgCalculatedSize.x) / 2;
+    newImgCenter.y = (image.clientHeight - newImgCalculatedSize.y) / 2;
+
+    setImgCalculatedSize(newImgCalculatedSize)
+    setImgCenter(newImgCenter)
+  }
 
 
   return (
     <div ref={containerRef} className={styles.container}>
       <img
+        id={imageId}
         className={styles.image + ' ' + styles.visible}
         src={apod.url}
         title={apod.title}
@@ -42,7 +102,15 @@ export const Showcase = (props: {
         target="_blank"
         rel="noreferrer"
       >
-        <div className={styles.content}>
+        <div
+          className={styles.content}
+          style={{
+            width: imgCalculatedSize.x + 'px',
+            height: imgCalculatedSize.y + 'px',
+            left: String(imgCenter.x + 10) + 'px',
+            top: String(imgCenter.y + 10) + 'px',
+          }}
+        >
           <div className={styles.title}>
             <h2>{apod.title}</h2>
             <p>By {apod.copyright}, {apod.date}</p>
